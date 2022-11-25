cc22365fa58ca1104a313a7b5af5b9c8263755c2
lukatercon <luka.tercon@gmail.com>
2022-11-25 19:02:50 +0100

    Add examples and improve script

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # tei2conllu
 
-Python script for converting TEI XML to CoNLL-U. This is still a bare-bones version, so it currently works only on a very specific type of TEI file. The goal is to eventually have a conversion script that works with the type of format used by the upcoming SUK corpus. 
+Python script for converting TEI XML to CoNLL-U. This is still a bare-bones version, so it currently works only on a very specific type of TEI file. The goal is to eventually have a conversion script that works with the type of format used by the upcoming SUK corpus.
 
 The script defaults to writing UD dependency links, but the "--syn-type JOS" option can be added to make it write JOS dependency links.
+
+Running through a command line:
+
+`TEI2conllu.py input_file.xml` for UD links
+
+`TEI2conllu.py input_file.xml --syn-type JOS` for JOS links
+
+Example input and output files can be found in the example/ directory. At the moment, only files with the same schema as the example input file will work.
--- TEI2conllu.py
+++ TEI2conllu.py
@@ -2,83 +2,116 @@ import conllu
 import xml.etree.ElementTree as ET
 import argparse
 
+
+# returns paragraph id
+def get_para_id(paragraph):
+    return paragraph.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+
+
+# returns document id
+def get_doc_id(doc):
+    return doc.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+
+
 # returns sentence id
-def getSentID(sentence):
+def get_sent_id(sentence):
     return sentence.attrib["{http://www.w3.org/XML/1998/namespace}id"]
 
-# returns sentence text
-def getSentText(sentence):
-    SentText = ""
-    for element in sentence:
-        if element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and "join" in element.attrib.keys():
-            SentText += element.text + ""
-        elif element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and not "join" in element.attrib.keys():
-            SentText += element.text + " "
-    SentText = SentText.rstrip()
-    return SentText
 
-# used in the writeElementsNoLinks function, splits up a token attribute and returns the upos and feats tags
-def SplitUpAttribute(attribute):
-    UposTag = attribute.split("|")[0][8:]
-    FeatsInString = attribute.split("|")[1:]
-    Feats = {}
-    for feature in FeatsInString:
+# returns sentence text
+def get_sent_text(sentence):
+    sent_text = ""
+    for element in sentence:
+        if element.tag == "{http://www.tei-c.org/ns/1.0}seg":
+            for word in element:
+                if "join" in word.attrib.keys():
+                    sent_text += word.text + ""
+                else:
+                    sent_text += word.text + " "
+        elif element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and "join" in element.attrib.keys():
+            sent_text += element.text + ""
+        elif element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and "join" not in element.attrib.keys():
+            sent_text += element.text + " "
+    sent_text = sent_text.rstrip()
+    return sent_text
+
+
+# used in the writeNoLinks function, splits up a token attribute and returns the upos and feats tags
+def split_up_attribute(attribute):
+    upos_tag = attribute.split("|")[0][8:]
+    feats_in_string = attribute.split("|")[1:]
+    feats = {}
+    for feature in feats_in_string:
         name, value = feature.split("=")
-        Feats[name] = value
-    return UposTag, Feats
+        feats[name] = value
+    return upos_tag, feats
+
+
+# used in write_elements. Checks if word is punctuation or word and then fills the conllu sentence
+# accordingly.
+def write_no_links(element, conllu_sentence):
+    if element.tag == "{http://www.tei-c.org/ns/1.0}w":
+
+        upos_tag, feats = split_up_attribute(element.attrib["msd"])
+
+        conllu_sentence.append({"id": int(element.attrib["{http://www.w3.org/XML/1998/namespace}id"].split(".")[-1][1:]),
+                               "form": element.text, "lemma": element.attrib["lemma"], "upos": upos_tag,
+                               "xpos": element.attrib["ana"][4:], "feats": feats, "head": None, "deprel": None,
+                               "deps": None, "misc": None})
+    elif element.tag == "{http://www.tei-c.org/ns/1.0}pc":
+
+        upos_tag = element.attrib["msd"].split("=")[1]
+
+        conllu_sentence.append({"id": int(element.attrib["{http://www.w3.org/XML/1998/namespace}id"].split(".")[-1][1:]),
+                               "form": element.text, "lemma": element.text, "upos": upos_tag,
+                               "xpos": element.attrib["ana"][4:], "feats": None, "head": None, "deprel": None,
+                               "deps": None, "misc": None})
+    if element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and "join" in element.attrib.keys():
+        conllu_sentence[-1]["misc"] = {"SpaceAfter": "No"}
+
 
 # takes a sentence from TEI and a sentence from conllu and fills the conllu sentence with all annotations except the
 # syntactic links
-def writeElementsNoLinks(TEISentence, ConlluSentence):
-    for element in TEISentence:
-        if element.tag == "{http://www.tei-c.org/ns/1.0}w":
+def write_elements(tei_sentence, conllu_sentence):
+    for element in tei_sentence:
+        if element.tag == "{http://www.tei-c.org/ns/1.0}seg":
+            for word in element:
+                write_no_links(word, conllu_sentence)
+        else:
+            write_no_links(element, conllu_sentence)
 
-            UposTag, Feats = SplitUpAttribute(element.attrib["msd"])
-
-            ConlluSentence.append({"id": int(element.attrib["{http://www.w3.org/XML/1998/namespace}id"].split(".")[-1]),
-                                   "form": element.text, "lemma": element.attrib["lemma"], "upos": UposTag,
-                                   "xpos": element.attrib["ana"][4:], "feats": Feats, "head": None, "deprel": None,
-                                   "deps": None, "misc": None})
-        elif element.tag == "{http://www.tei-c.org/ns/1.0}pc":
-
-            UposTag = element.attrib["msd"].split("=")[1]
-
-            ConlluSentence.append({"id": int(element.attrib["{http://www.w3.org/XML/1998/namespace}id"].split(".")[-1]),
-                                   "form": element.text, "lemma": element.text, "upos": UposTag,
-                                   "xpos": element.attrib["ana"][4:], "feats": None, "head": None, "deprel": None,
-                                   "deps": None, "misc": None})
-        if element.tag != "{http://www.tei-c.org/ns/1.0}linkGrp" and "join" in element.attrib.keys():
-            ConlluSentence[-1]["misc"] = {"SpaceAfter": "No"}
 
 # takes a sentence from TEI and a sentence from conllu and fills the conllu sentence with UD links
-def writeUDSynLinks(TEIsentence, ConlluSentence):
-    for element in TEIsentence:
+def write_ud_links(tei_sentence, conllu_sentence):
+    for element in tei_sentence:
         if element.tag == "{http://www.tei-c.org/ns/1.0}linkGrp" and element.attrib["type"] == "UD-SYN":
             for link in element:
-                LinkName = link.attrib["ana"][7:]
+                link_name = link.attrib["ana"][7:]
                 head, dependent = link.attrib["target"].split(" ")
-                if LinkName == "root":
+                if link_name == "root":
                     head = 0
                 else:
-                    head = int(head.split(".")[-1])
-                dependent = int(dependent.split(".")[-1]) - 1
-                ConlluSentence[dependent]["head"] = head
-                ConlluSentence[dependent]["deprel"] = LinkName
+                    head = int(head.split(".")[-1][1:])
+                dependent = int(dependent.split(".")[-1][1:]) - 1
+                conllu_sentence[dependent]["head"] = head
+                conllu_sentence[dependent]["deprel"] = link_name
+
 
 # takes a sentence from TEI and a sentence from conllu and fills the conllu sentence with JOS links
-def writeJOSSynLinks(TEIsentence, ConlluSentence):
-    for element in TEIsentence:
+def write_jos_links(tei_sentence, conllu_sentence):
+    for element in tei_sentence:
         if element.tag == "{http://www.tei-c.org/ns/1.0}linkGrp" and element.attrib["type"] == "JOS-SYN":
             for link in element:
-                LinkName = link.attrib["ana"][8:]
+                link_name = link.attrib["ana"][8:]
                 head, dependent = link.attrib["target"].split(" ")
-                if LinkName == "modra":
+                if link_name == "modra":
                     head = 0
                 else:
-                    head = int(head.split(".")[-1])
-                dependent = int(dependent.split(".")[-1]) - 1
-                ConlluSentence[dependent]["head"] = head
-                ConlluSentence[dependent]["deprel"] = LinkName
+                    head = int(head.split(".")[-1][1:])
+                dependent = int(dependent.split(".")[-1][1:]) - 1
+                conllu_sentence[dependent]["head"] = head
+                conllu_sentence[dependent]["deprel"] = link_name
+
 
 # main part of the script
 # argparse
@@ -92,32 +125,46 @@ args = argparser.parse_args()
 tree = ET.parse(args.file)
 root = tree.getroot()
 
+#check if TEI file has the right sequence of elements
+if root[0].tag == "{http://www.tei-c.org/ns/1.0}bibl" and root[1].tag == "{http://www.tei-c.org/ns/1.0}p":
+    print("Sequence of elements in file is OK")
+else:
+    raise Exception("The sequence of elements in the TEI XML is not supported")
+
 # prepare list of conllu sentences
 ConlluSentences = []
 
-# go through all the TEI sentences and write all the data of each sentence to a new conllu TokenList object. At the end,
-# append the TokenList to the list of conllu sentences
-for body in root[1]:
-    for paragraph in body:
-        for sentence in paragraph:
-            SentToWrite = conllu.TokenList()
+# go through all the TEI sentences and write all the data from each sentence to a new conllu TokenList object. At the
+# end, append the TokenList to the list of conllu sentences
+doc_id = get_doc_id(root)
+for ele in root:
+    if ele.tag == "{http://www.tei-c.org/ns/1.0}p":
+        para_id = get_para_id(ele)
+        for tei_sent in ele:
+            sent_to_write = conllu.TokenList()
 
-            SentID = getSentID(sentence)
-            SentToWrite.metadata.update({"sent_id": SentID})
+            if tei_sent == root[1][0]:
+                sent_to_write.metadata.update({"newdoc id": doc_id})
 
-            SentText = getSentText(sentence)
-            SentToWrite.metadata.update({"text": SentText})
+            if tei_sent == ele[0]:
+                sent_to_write.metadata.update({"newpar id": para_id})
 
-            writeElementsNoLinks(sentence, SentToWrite)
+            sent_id = get_sent_id(tei_sent)
+            sent_to_write.metadata.update({"sent_id": sent_id})
+
+            tei_sent_text = get_sent_text(tei_sent)
+            sent_to_write.metadata.update({"text": tei_sent_text})
+
+            write_elements(tei_sent, sent_to_write)
 
             if args.syn == "UD":
-                writeUDSynLinks(sentence, SentToWrite)
+                write_ud_links(tei_sent, sent_to_write)
             else:
-                writeJOSSynLinks(sentence, SentToWrite)
+                write_jos_links(tei_sent, sent_to_write)
 
-            ConlluSentences.append(SentToWrite)
+            ConlluSentences.append(sent_to_write)
 
 # go through the conllu sentence list and write to a .conllu file
 with open(f"{args.file[:-3]}conllu", "w", encoding="UTF-8") as write_file:
-    for SentInConllu in ConlluSentences:
-        write_file.write(SentInConllu.serialize())
+    for sent_in_conllu in ConlluSentences:
+        write_file.write(sent_in_conllu.serialize())
